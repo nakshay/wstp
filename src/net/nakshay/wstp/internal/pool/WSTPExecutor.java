a3edef4b116602a42472b7cc39753158f2bca79c
Akshay Naik <inbox.akshaynaik@gmail.com>
2020-11-14 10:13:40 +0530

    Added initial logic to submit and start the task

--- src/net/nakshay/wstp/internal/pool/WSTPExecutor.java
+++ src/net/nakshay/wstp/internal/pool/WSTPExecutor.java
@@ -16,6 +16,22 @@ class WSTPExecutor implements WSTPService {
 
   @Override
   public void runJob(Runnable runnable) {
-    
+
+    // Create worker, and each worker will have its own queue
+    // add task to the queue as per the load in the queue
+    // as queue becomes empty worker will steal queue from another worker
+
+    WorkStealThreadPool.Worker[] workers = threadPool.getWorkers();
+    boolean submitted = false;
+    for (WorkStealThreadPool.Worker worker : workers) {
+      if (worker.queueSize() < 1) {
+        worker.addTask(runnable);
+        submitted = true;
+      }
+    }
+
+    if (!submitted) {
+      // need to figure out what to be done for tasks which could not be submitted
+    }
   }
 }
--- src/net/nakshay/wstp/internal/pool/WorkStealThreadPool.java
+++ src/net/nakshay/wstp/internal/pool/WorkStealThreadPool.java
@@ -1,40 +1,56 @@
 package net.nakshay.wstp.internal.pool;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 class WorkStealThreadPool {
 
-  private static int DEFAULT_POOL_SIZE;
-  private static int MAX_PARALLELISM;
-
-  static {
-    MAX_PARALLELISM = Runtime.getRuntime().availableProcessors();
-    DEFAULT_POOL_SIZE = MAX_PARALLELISM;
-  }
+  private final Worker[] workers;
 
   WorkStealThreadPool(final int poolSize) {
     validatePoolSize(poolSize);
-    DEFAULT_POOL_SIZE = poolSize;
+    workers = new Worker[poolSize];
+  }
+
+  WorkStealThreadPool() {
+    this(Runtime.getRuntime().availableProcessors());
   }
 
   private void validatePoolSize(final int poolSize) {
+    int MAX_PARALLELISM = Runtime.getRuntime().availableProcessors();
+
     if (poolSize > MAX_PARALLELISM) {
       throw new IllegalArgumentException();
     }
   }
 
-  WorkStealThreadPool() {
-    this(10);
+  protected Worker[] getWorkers() {
+    return workers;
   }
 
-  public class Worker implements Runnable {
+  public static class Worker implements Runnable {
+    // Default size for each queue 10 for now, need to think on this.
+    private Deque<Runnable> taskQueue = new ArrayDeque<>(10);
 
-    private Runnable task = null;
-    private Thread thread = null;
+    protected int queueSize() {
+      return taskQueue.size();
+    }
 
-    Worker(Runnable task) {
-      this.task = task;
+    protected void addTask(Runnable task) {
+      taskQueue.offerLast(task);
+    }
+
+    protected Runnable stealTask() {
+      return taskQueue.pollLast();
     }
 
     @Override
-    public void run() {}
+    public void run() {
+      // need to decide on logic for polling tasks from queue
+      while (true) {
+        Runnable runnable = taskQueue.pollFirst();
+        new Thread(runnable).start();
+      }
+    }
   }
 }
